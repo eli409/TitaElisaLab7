b187aee8cba1cebfc44428ac63764fa6444378d6
Diana Elisa Tita <diana.elisa.tita@gmail.com>
2025-11-28 17:46:57 +0200

    Lab8 - final

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace TitaElisaLab7
+﻿using System;
+using TitaElisaLab7.Data;
+using System.IO;
+
+namespace TitaElisaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new 
+                        ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                        LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ ListPage.xaml.cs
@@ -0,0 +1,24 @@
+using TitaElisaLab7.Models;
+namespace TitaElisaLab7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+
+	async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		var slist = (ShopList)BindingContext;
+		slist.Date = DateTime.UtcNow;
+		await App.Database.SaveShopListAsync(slist);
+		await Navigation.PopAsync();
+	}
+	async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		var slist = (ShopList)BindingContext;
+		await App.Database.DeleteShopListAsync(slist);
+		await Navigation.PopAsync();
+	}
+}
